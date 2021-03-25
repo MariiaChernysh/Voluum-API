b474aef81c2d5996ff146c8749f0a921143e7901
Darina Ponomarova <64550850+ddarenka@users.noreply.github.com>
2021-03-25 18:32:02 +0200

    Weekly data request

--- LTD_Request.py
+++ LTD_Request.py
@@ -24,8 +24,9 @@ print(response.text)
 ttt = json.loads(response.text)
 token = ttt.get('token')
 
-start_date = '2019-01-01'
-end_date = '2021-03-16'
+date_today = date.today()
+start_date = date_today - timedelta(days=7)
+
 
 date_i = pd.to_datetime(start_date)
 # start_month = start_date.month
@@ -49,7 +50,7 @@ while not stop:
 
     for cal_date in cal_iter:
         if cal_date.month == iter_month:
-            if cal_date == date.today() - timedelta(days=1):
+            if cal_date >= start_date:
                 headers = {
                     'authority': 'panel-api2.voluum.com',
                     'sec-ch-ua': '"Chromium";v="88", "Google Chrome";v="88", ";Not A Brand";v="99"',
@@ -98,56 +99,11 @@ while not stop:
 
                 daily_result = [json.dumps(record) for record in jsn_data['rows']]
                 total_result = total_result + daily_result
-                stop = True
-                break
-            headers = {
-                'authority': 'panel-api2.voluum.com',
-                'sec-ch-ua': '"Chromium";v="88", "Google Chrome";v="88", ";Not A Brand";v="99"',
-                'cwauth-token': token,
-                'sec-ch-ua-mobile': '?0',
-                'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.190 Safari/537.36',
-                # 'cwauth-panel-token': '80CxSnKulRSW1yigOOrkyH7ublIz02Sf',
-                'content-type': 'application/json',
-                'accept': 'application/json',
-                'clientid': '08148d1f-a3d6-4323-8f64-6080302fc87f',
-                'origin': 'https://panel.voluum.com',
-                'sec-fetch-site': 'same-site',
-                'sec-fetch-mode': 'cors',
-                'sec-fetch-dest': 'empty',
-                'referer': 'https://panel.voluum.com/',
-                'accept-language': 'en-US,en;q=0.9,es;q=0.8',
-            }
-
-            params = (
-                ('from', str(cal_date) + 'T00:00:00Z'),
-                ('to', str(cal_date + timedelta(days=1)) + 'T00:00:00Z'),
-                ('tz', 'Europe/London'),
-                ('conversionTimeMode', 'CONVERSION'),
-                ('currency', 'USD'),
-                ('sort', 'cost'),
-                ('direction', 'desc'),
-                ('column',
-                 ['countryName', 'visits', 'visits', 'clicks', 'clicks', 'customConversions1', 'customConversions2',
-                  'revenue',
-                  'cost']),
-                ('groupBy', 'country-code'),
-                ('offset', '0'),
-                ('limit', '10000'),
-                ('include', 'ACTIVE'),
-            )
-
-            response = requests.get('https://panel-api2.voluum.com/report', headers=headers, params=params)
-
-            data = response.text
-
-            jsn_data = json.loads(data)
-            for row in jsn_data['rows']:
-                row['date'] = str(cal_date)
-
-            print('Received the requested LTD-report data for: ' + str(cal_date))
-
-            daily_result = [json.dumps(record) for record in jsn_data['rows']]
-            total_result = total_result + daily_result
+                
+                if cal_date == date_today:
+                    stop = True
+                    break
+          
         elif cal_date.month == iter_month + 1:
             break
 
@@ -158,6 +114,7 @@ while not stop:
     iter_month = iter_month + 1
 
 print('The request is 100% done')
+
 with open("sample.json", "w") as outfile:
     for i in total_result:
         outfile.write(i+'\n')
